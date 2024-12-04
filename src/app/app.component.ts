92116b1d1e8ce6071a572e0a76a359b0fe14157c
Reemhossam <reemhossam1997@gmail.com>
2024-12-04 19:03:41 +0200

    add reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularReactiveForm';
+  registerForm: FormGroup;
+
+  ngOnInit(){
+    this.registerForm = new FormGroup({
+      firstname: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required,Validators.email]),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+      address: new FormGroup({
+        street: new FormControl(null, Validators.required),
+        country: new FormControl('India'),
+        city: new FormControl(null),
+        region: new FormControl(null, Validators.required),
+        postal: new FormControl(null, Validators.required)}),
+      skills: new FormArray ([
+        new FormControl(null),
+        new FormControl(null),
+        new FormControl(null)
+      ])
+      })
+  }
+
+  onSubmit() {
+    console.log('Your form data:', this.registerForm);
+  }
 }
